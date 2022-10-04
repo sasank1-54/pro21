459d78b3583bcb44e00daea4dccc39b94b21d5b2
sasank1-54 <114579796+sasank1-54@users.noreply.github.com>
2022-10-04 15:41:06 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,59 @@
+//Arithematic operator
+
+let a = 90;
+let b = 40;
+let c = a+b;
+let c1 = a-b;
+let c2 = a*b;
+let c3 = a/b;
+let c4 = a%b;
+let c5 = a**2;
+
+console.log(c, c1, c2, c3, c4, c5);
+
+//Logical
+
+let c6 = a||b;
+let c7= a&&b;
+let c8 = !b;
+console.log(c6, c7,c8);
+
+//Bitwise
+
+let c9 = a|b;
+let c10 = a&b;
+let c11 = a^b;
+let c12= a<<2;
+let c13 = b>>2;
+let c14 =  a ?? b; 
+
+console.log(c9, c10, c11, c12, c13,c14);
+
+//Comparision 
+
+let c15 = a>b;
+let c16 = a<b;
+let c17 = a==b;
+let c18= a!=b;
+let c19= a>=b;
+let c20= a<=b;
+
+console.log(c15,c16,c17,c18,c19,c20);
+
+//Terinary Operator
+
+let t1 = a>b ? a:b;
+
+console.log(t1)
+
+
+//Increment Decrement
+
+let c21= a++;
+let c22= a--;
+let c33 = b++;
+let c44= b--;
+
+console.log(c21,c22,c33,c44);
+
+
